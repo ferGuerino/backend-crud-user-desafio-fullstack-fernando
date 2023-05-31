3d439f71c65d5a5911586d65a3528f917e00d084
ferGuerino <fernando.guerino2014@gmail.com>
2023-05-31 15:42:24 -0300

    Feat: adicionado cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata"
 import "express-async-errors"
 import express from "express"
+import cors from "cors"
 import { usersRoutes } from "./routes/users.route"
 import { handleErrorMiddleware } from "./middlewares/handleAppError.middleware"
 import { loginRoutes } from "./routes/login.route"
@@ -9,6 +10,7 @@ import swaggerUi from "swagger-ui-express"
 import swaggerDocument from "../swagger.json"
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use("/users", usersRoutes)
